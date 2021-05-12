313bb45b8d74417fcabe5c2b49eec382fbe03a5e
Korbs <KorbsStudio@OnMail.com>
2021-05-12 11:59:01 -0400

    Merge Auto Detect OS Titlebar

--- main.js
+++ main.js
@@ -2,22 +2,74 @@ const {app, BrowserWindow, Menu, protocol, ipcMain} = require('electron');
 const log = require('electron-log');
 const {autoUpdater} = require("electron-updater");
 
-
 let win;
+
+var osvar = process.platform; /* Detecting OS */
+if (osvar == 'darwin') {app.whenReady().then(() => {createWindowMac()})
+}else if(osvar == 'win32'){app.whenReady().then(() => {createWindowWin()})
+}else{app.whenReady().then(() => {createWindowLinux()})}
+
 
 function sendStatusToWindow(text) {
   log.info(text);
   win.webContents.send('message', text);
 }
-function createDefaultWindow() {
-  win = new BrowserWindow();
-  win.webContents;
-  win.on('closed', () => {
-    win = null;
-  });
-  win.loadURL(`file://${__dirname}/version.html#v${app.getVersion()}`);
-  return win;
+
+
+
+function createWindowWin () { /* Windows */
+  const mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    frame: false,
+    closable: true,
+    maximizable: true,
+    minimizable: true,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+      nodeIntegration: true
+    }
+  })
+  mainWindow.loadURL(`file://${__dirname}/index.html#v${app.getVersion()}`);
 }
+function createWindowMac () { /* macOS */
+  const mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    frame: true,
+    closable: true,
+    maximizable: true,
+    minimizable: true,
+    titleBarStyle: 'hiddenInset',
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+      nodeIntegration: true
+    }
+  })
+  mainWindow.loadURL(`file://${__dirname}/index.html#v${app.getVersion()}`);
+}
+function createWindowLinux () { /* Linux */
+  const mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    frame: true,
+    closable: true,
+    maximizable: true,
+    minimizable: true,
+    autoHideMenuBar: true,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  })
+  mainWindow.loadURL(`file://${__dirname}/index.html#v${app.getVersion()}`);
+  mainWindow.webContents.on('did-finish-load', function() {
+    mainWindow.webContents.insertCSS('#titlebar{display: none !important;}') /* Remove Windows Titlebar if OS is Linux */
+ });
+}
+
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') app.quit()
+})
 
 autoUpdater.on('checking-for-update', () => {
   sendStatusToWindow('Checking for update...');
@@ -38,19 +90,3 @@ autoUpdater.on('download-progress', (progressObj) => {
   log_message = log_message + ' (' + progressObj.transferred + "/" + progressObj.total + ')';
   sendStatusToWindow(log_message);
 })
-
-autoUpdater.on('update-downloaded', (info) => {
-  sendStatusToWindow('Update downloaded');
-});
-
-app.on('ready', function() {
-  // Create the Menu
-  createDefaultWindow();
-});
-app.on('window-all-closed', () => {
-  app.quit();
-});
-
-app.on('ready', function()  {
-  autoUpdater.checkForUpdatesAndNotify();
-});

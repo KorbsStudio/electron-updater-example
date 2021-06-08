b86e59657514144705e2fdc1d9125d8c625f6934
Korbs <KorbsStudio@OnMail.com>
2021-06-08 01:10:10 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,53 +1,14 @@
-const { app, BrowserWindow, ipcMain } = require('electron');
-const { autoUpdater } = require('electron-updater');
-
-let mainWindow;
+const {app, BrowserWindow} = require('electron')
 
 function createWindow () {
-  mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
-    webPreferences: {
-      nodeIntegration: true,
-    },
-  });
-  mainWindow.loadFile('index.html');
-  mainWindow.on('closed', function () {
-    mainWindow = null;
-  });
-  mainWindow.once('ready-to-show', () => {
-    autoUpdater.checkForUpdatesAndNotify();
-  });
+  // Show and control the main window of your application
+  // Electron Docs: https://www.electronjs.org/docs/api/browser-window#class-browserwindow
+  const mainWindow = new BrowserWindow({
+    width: 600,
+    height: 400,
+    autoHideMenuBar: true
+  })
+  mainWindow.loadFile('index.html')
+  autoUpdater.checkForUpdatesAndNotify();
 }
-
-app.on('ready', () => {
-  createWindow();
-});
-
-app.on('window-all-closed', function () {
-  if (process.platform !== 'darwin') {
-    app.quit();
-  }
-});
-
-app.on('activate', function () {
-  if (mainWindow === null) {
-    createWindow();
-  }
-});
-
-ipcMain.on('app_version', (event) => {
-  event.sender.send('app_version', { version: app.getVersion() });
-});
-
-autoUpdater.on('update-available', () => {
-  mainWindow.webContents.send('update_available');
-});
-
-autoUpdater.on('update-downloaded', () => {
-  mainWindow.webContents.send('update_downloaded');
-});
-
-ipcMain.on('restart_app', () => {
-  autoUpdater.quitAndInstall();
-});
+app.whenReady().then(() => {createWindow()})
